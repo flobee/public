d3b91fd48ed6f41a7a6ba1d342b9a1ce6558d63b
flobee <flobee@gmail.com>
2015-09-02 17:28:06 +0200

    Update config.inc.php

--- etc/phpmyadmin/config.inc.php
+++ etc/phpmyadmin/config.inc.php
@@ -1,1 +1,25 @@
 <?php
+
+// bring back my old fashion style
+
+$cfg['NumRecentTables'] = 20;
+$cfg['NumFavoriteTables'] = 20;
+$cfg['QueryHistoryMax'] = 50;
+$cfg['MaxCharactersInDisplayedSQL'] = 1000;
+$cfg['NavigationDisplayLogo'] = false;
+$cfg['MaxNavigationItems'] = 300;
+$cfg['FirstLevelNavigationItems'] = 100;
+$cfg['NavigationTreeEnableGrouping'] = false;
+$cfg['NavigationTreeDisplayItemFilterMinimum'] = 100;
+$cfg['NavigationTreeDisplayDbFilterMinimum'] = 100;
+$cfg['ShowPhpInfo'] = true;
+$cfg['ActionLinksMode'] = 'icons';
+//$cfg['ShowDbStructureCreation'] = true;
+//$cfg['ShowDbStructureLastUpdate'] = true;
+//$cfg['ShowDbStructureLastCheck'] = true;
+$cfg['MaxRows'] = 50;
+$cfg['Import']['charset'] = 'utf-8';
+$cfg['Import']['csv_terminated'] = ';';
+$cfg['Import']['ods_col_names'] = true;
+$cfg['Export']['csv_separator'] = ';';
+$cfg['Export']['excel_columns'] = true;
