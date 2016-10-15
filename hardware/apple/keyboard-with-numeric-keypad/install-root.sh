ccdfc7e3b6efda2e88520efeda67f1ab47ff06ca
flobee <flobee@r61>
2016-10-15 17:56:08 +0200

    Updates install script for mac keyboard

--- hardware/apple/keyboard-with-numeric-keypad/install-root.sh
+++ hardware/apple/keyboard-with-numeric-keypad/install-root.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
 #
-# Toogle F<N> and multimedia keys
-#
-echo "options hid_apple fnmode=2" >> /etc/modprobe.d/hid_apple.conf
-sudo update-initramfs -u -k all
+echo "Permanetly changes for apple keyboard with numeric pad, german keyboard";
 
+echo "Run this script when changing or update the kernel to reactivate this";
+echo "settings. Note: Settings will be repalaced. Dont edit target file or";
+echo "change this script!";
+
+# Toogles F<N> and multimedia keys
+# manually: echo 2 > /sys/module/hid_apple/parameters/fnmode
+echo "Toogles F<N> and multimedia keys permanetly";
+echo 'run: echo "options hid_apple fnmode=2" > /etc/modprobe.d/hid_apple.conf';
+echo "options hid_apple fnmode=2" > /etc/modprobe.d/hid_apple.conf
+
+
+# tauschen der alt + cmd tasten, zwar richtig bezeichnet aber unterschiedlich 
+# zur pc tastatur
+# cmd: echo 1 > /sys/module/hid_apple/parameters/swap_opt_cmd
+# options hid_apple swap_opt_cmd=1
+
+
+echo 'Update initramfs';
+sudo update-initramfs -u -k all;
+
+
+echo 'Done.';
 
