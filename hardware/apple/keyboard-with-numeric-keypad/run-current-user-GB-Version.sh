21ce7065f5bccdb41ef2297a97ccd1a68aba3b9a
flobee <flobee@r61>
2016-12-26 17:35:03 +0100

    adds more options

--- hardware/apple/keyboard-with-numeric-keypad/run-current-user-GB-Version.sh
+++ hardware/apple/keyboard-with-numeric-keypad/run-current-user-GB-Version.sh
@@ -1,17 +1,35 @@
-#!/bin/shi
-# 
-# Helper:
-# xev | grep keycode
-#
-#
-# Toggle <> and ^° keys, DE version
-# xmodmap \
-#    -e 'keycode 49 = less greater less greater bar brokenbar bar' \
-#    -e 'keycode 94 = dead_circumflex degree dead_circumflex degree U2032 U2033 U2032'
+#!/bin/sh
 
+# configuration file for xmodmap using on debian 8
+
+# Helper:
+# https://wiki.archlinux.org/index.php/xmodmap
 #
+# xev | grep keycode
+
+
 # Have a X Menu on right and left cmd key
+# keycode 133 = Menu
+# keycode 134 = Menu'
+
+
+# toggle from QWERTY to QWERTZ so Z vs. Y
 xmodmap \
-    -e 'keycode 133 = Menu' \
-    -e 'keycode 134 = Menu'
+    -e 'keycode 29 = z Z z Z z Z z Z' \
+    -e 'keycode 52 = y Y y Y y Y y Y' \
+
+
+# euro sign at the e and q key using right alt key (alt-gr)
+xmodmap \
+    -e 'keycode 24 = q Q EuroSign cent EuroSign cent EuroSign cent' \
+    -e 'keycode 26 = e E EuroSign cent EuroSign cent EuroSign cent'
+
+
+# umlaute
+# keycode 30 = u U udiaeresis Udiaeresis udiaeresis Udiaeresis
+# keycode 32 = o O odiaeresis Odiaeresis odiaeresis Odiaeresis
+# keycode 38 = a A adiaeresis Adiaeresis adiaeresis Adiaeresis
+
+
+
 
