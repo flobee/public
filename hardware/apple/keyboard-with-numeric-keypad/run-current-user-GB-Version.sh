202e0929efe1e3aa63e3922acdb3e3e856f04009
flobee <flobee@r61>
2016-12-27 01:07:37 +0100

    gb version for german users (zy to yz)

--- hardware/apple/keyboard-with-numeric-keypad/run-current-user-GB-Version.sh
+++ hardware/apple/keyboard-with-numeric-keypad/run-current-user-GB-Version.sh
@@ -1,35 +1,31 @@
 #!/bin/sh
 
-# configuration file for xmodmap using on debian 8
-
 # Helper:
 # https://wiki.archlinux.org/index.php/xmodmap
 #
 # xev | grep keycode
-
-
+#
 # Have a X Menu on right and left cmd key
 # keycode 133 = Menu
 # keycode 134 = Menu'
+#
+# configuration file for xmodmap using on debian 8
+
+lc='GB';
+tmpfile='.xmodmap.tmp';
+configfile="xmodmap.$lc.cfg";
 
 
-# toggle from QWERTY to QWERTZ so Z vs. Y
-xmodmap \
-    -e 'keycode 29 = z Z z Z z Z z Z' \
-    -e 'keycode 52 = y Y y Y y Y y Y' \
+if [ -f $configfile ] ;
+then
+    # drop comments
+    sed -e '/^\s*$/d' -e '/^#/d' -e '/^;/d' $configfile > $tmpfile
+else
+    echo "error";
+fi
+
+# sourece generatet config file
+xmodmap .xmodmap.tmp
 
 
-# euro sign at the e and q key using right alt key (alt-gr)
-xmodmap \
-    -e 'keycode 24 = q Q EuroSign cent EuroSign cent EuroSign cent' \
-    -e 'keycode 26 = e E EuroSign cent EuroSign cent EuroSign cent'
-
-
-# umlaute
-# keycode 30 = u U udiaeresis Udiaeresis udiaeresis Udiaeresis
-# keycode 32 = o O odiaeresis Odiaeresis odiaeresis Odiaeresis
-# keycode 38 = a A adiaeresis Adiaeresis adiaeresis Adiaeresis
-
-
-
-
+exit;
