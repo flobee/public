24222fe54c5c04f11c92d77967540d326d4d386e
flobee <flobee@debian>
2018-04-09 21:42:25 +0200

    Improves script

--- shell/mysql_mariadb/backup_dbs_root.sh
+++ shell/mysql_mariadb/backup_dbs_root.sh
@@ -2,19 +2,26 @@
 
 # backup each database in a seperated directory
 #
-# Install mydumper (apt-get install mydumper)
-# use myloader to restore stuff, eg: 
+# Install "mydumper" (apt-get install mydumper)
+# use "myloader" to restore stuff, eg:
 # myloader -u ${MYSQL_USER} -p ${MYSQL_PASS} -h ${MYSQL_HOST} \
 #   --port=${MYSQL_PORT} ${MYSQL_SOCK} --overwrite-tables --threads 4 \
 #   --database ${DBNAME}  --directory "${OUTDIR}/${DBNAME}"
 #
 #
-# first parameter as directory prefix which will creates:
-# eg: <script> /tmp/somepath will result in
+# first parameter as directory or as config file prefix.
+# As, with given directory (manual backup), will create:
+# eg: "<script> /tmp/somepath" will result in:
 # /tmp/somepath/<database-name>/<sql files of tables and schemas>
-# otherwise location to an individual config including the parameters: 
+# or, otherwise location to an individual config including the parameters:
 #   MYSQL_USER, MYSQL_PASS, MYSQL_PORT, MYSQL_HOST, OUTDIR
+# to be run as cron job.
+# if you only want to run this script in cron you can alter the shebang to (a)sh
+# or dash
 #
+# SECURITY WARNING: Use this script only in you private hood e.g. using remote 
+# connections/ private network. Otherwise your users can see your credentials 
+# eg. via process list! I opened a ticket for this issue already.
 
 
 # pipe individual setups to the script for cron jobs
@@ -32,9 +39,10 @@ if [ -d "$CONFFILE" -o "$CONFFILE" = "" ]; then
     _MYSQL_PASS='';
     _MYSQL_PORT='3306';
     _MYSQL_HOST='localhost';
-    # depending on your setup, eg. synology mariadb5 and 10 need a socket or you 
+    # depending on your setup, eg. synology mariadb5 and 10 need a socket or you
     # get only v5 even if the port if different
-    _MYSQL_SOCK=''; #'-S /run/mysqld/mysqld10.sock'; # set to '' to not use socket connection
+    # set to '' to not use sockets
+    _MYSQL_SOCK=''; #'-S /run/mysqld/mysqld10.sock';
 
     read -p "Enter MYSQL USER (default: '$_MYSQL_USER'): " MYSQL_USER
     MYSQL_USER=${MYSQL_USER:-$_MYSQL_USER}
@@ -53,6 +61,7 @@ if [ -d "$CONFFILE" -o "$CONFFILE" = "" ]; then
 else 
     source $CONFFILE
     if [ ! -d "$OUTDIR" ]; then
+        # do not -p if the path belongs to a network fs
         mkdir "$OUTDIR"
     fi
 fi
@@ -61,13 +70,12 @@ MYSQL_CONN="-u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} --port=${MYSQL_PORT}
 MYDUMPER_CONN="-u ${MYSQL_USER} -p ${MYSQL_PASS} -h ${MYSQL_HOST} --port=${MYSQL_PORT} ${MYSQL_SOCK}"
 
 
-
 # Collect all database names except for
-# mysql, information_schema, and performance_schema and phpmyadmin
+# mysql, information_schema, performance_schema and phpmyadmin
 SQL="SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN"
 SQL="${SQL} ('mysql','information_schema','performance_schema', 'phpmyadmin')"
 
-HASH=`date +"%Y-%m-%d_%_H%M%S"`;
+HASH=`date +"%Y%m%d%_H%M%S"`;
 DBLISTFILE=/tmp/${HASH}DatabasesToDump.txt
 mysql ${MYSQL_CONN} -ANe"${SQL}" > ${DBLISTFILE}
 
@@ -78,10 +86,10 @@ do
 done
 
 
-echo "dump output to '${OUTDIR}/<database-name>/<sql-files>.sql'";
-echo "backup dbs: ${DBLIST}";
-
-echo "job starts in 3 sec... to abort press CNTL + C";
+echo "Backup goes to: '${OUTDIR}/<database-name>/<sql-files>.sql'";
+echo "List of DBs to backup: ${DBLIST}";
+echo
+echo "job starts in 3 sec... to abort press CTRL + C";
 sleep 3;
 
 MYSQLDUMP_OPTIONS="--routines --triggers --events --lock-all-tables --rows 1000000 --statement-size 100000"
@@ -92,9 +100,11 @@ for DBNAME in `cat ${DBLISTFILE}` ;
 do
     echo "Dumping: '${DBNAME}' to '${OUTDIR}/${DBNAME}'"
     mydumper ${MYDUMPER_CONN} ${MYSQLDUMP_OPTIONS} --database "${DBNAME}" --outputdir "${OUTDIR}/${DBNAME}"
-    # mysqldump ${MYSQL_CONN} ${MYSQLDUMP_OPTIONS} ${DBNAME} > "${OUTDIR}.${DBNAME}.sql"
+    # mysqldump ${MYSQL_CONN} ${MYSQLDUMP_OPTIONS} ${DBNAME} > "${OUTDIR}/${DBNAME}.sql"
 done
 
 
 # clean up
 rm $DBLISTFILE
+
+exit 0;
