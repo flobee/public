1c1a4dbec340d3bc19b8b3427404261d9d5958ac
flobee <flobee@debian.local>
2019-12-30 21:25:20 +0100

    Adds boinc install

---
+++ etc/boinc-client/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# systemctl stop boinc-client.service
+# cd /tmp;
+# wget -N "https://github.com/flobee/public/blob/master/etc/boinc-client/account_setiathome.berkeley.edu.xml";
+# cp -f account_setiathome.berkeley.edu.xml /etc/boinc-client/
+# systemctl start boinc-client.service
+
+
