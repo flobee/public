838bcc033f509262b35e7157f7bb824af0b7cf3e
flobee <flobee@debian.local>
2021-11-19 23:46:25 +0100

    Updates backup filename structure

--- shell/gitea/install/backup.sh
+++ shell/gitea/install/backup.sh
@@ -13,12 +13,12 @@ if [ ! -d "${PATH_BACKUPS}" ]; then
 fi
 
 echo '# pls delete old backups, probably:'
-ls -alh ${PATH_BACKUPS}/${DIRNAME_REPOSITORIES}-*;
-ls -alh ${PATH_BACKUPS}/${DIRNAME_GITEA}-*;
+ls -alh ${PATH_BACKUPS}/gitea-${DIRNAME_REPOSITORIES}-*;
+ls -alh ${PATH_BACKUPS}/gitea-${DIRNAME_GITEA}-*;
 
 echo '# creating backups, pls. wait...';
-tar -czf ${PATH_BACKUPS}/${DIRNAME_REPOSITORIES}-${TIMENOW}.tgz ${PATH_REPOSITORIES}/;
-tar -czf ${PATH_BACKUPS}/${DIRNAME_GITEA}-${TIMENOW}.tgz ${PATH_GITEA}/;
-echo "# Backups created: ${DIRNAME_REPOSITORIES}-${TIMENOW}.tgz, ${DIRNAME_GITEA}-${TIMENOW}.tgz";
+tar -czf ${PATH_BACKUPS}/gitea-${DIRNAME_REPOSITORIES}-${TIMENOW}.tgz ${PATH_REPOSITORIES}/;
+tar -czf ${PATH_BACKUPS}/gitea-${DIRNAME_GITEA}-${TIMENOW}.tgz ${PATH_GITEA}/;
+echo "# Backups created: gitea-${DIRNAME_REPOSITORIES}-${TIMENOW}.tgz, gitea-${DIRNAME_GITEA}-${TIMENOW}.tgz";
 echo
 
