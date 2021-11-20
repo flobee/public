dc875751e9458c11357975beaaf76308eac7a117
flobee <flobee@debian.local>
2021-11-20 02:07:02 +0100

    Updates inline docs

--- shell/gitea/install/config.sh
+++ shell/gitea/install/config.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-#
-# --- Paths and settings you may want to change: -----------------------
+###
+# Paths and settings you may want to change
 #
 
+
+# the user for this installation (eg. the 'git' user in/for: /home/git)
 USER=git;
 
+# the port where gitea should be available
 PORT=3001;
 
 PATH_HOME='/home/git';                      # home of $USER
@@ -16,18 +19,9 @@ PATH_GITEA="$PATH_HOME/tea";                # gitea in /home/git/tea/
 
 PATH_REPOSITORIES="$PATH_HOME/repositories" # repos in /home/git/repositories/
 
-GITEA_INSTALLER_URL='https://raw.githubusercontent.com/flobee/public/master/shell/gitea/INSTALL_gitea_README.md.sh';
+ACTION_BACKUPDEF='Y';                       # default value for creating backups first: Y|N
 
-# not yet
-#GITEA_INSTALLER_SCRIPT="${PATH_HOME}/gitea_install_update.sh";
-
-# not yet
-#ACTION_DROPSCRIPTDEF='N';                   # this script to be droped at the
-#                                            # remote side after execution; default: N (no)
-
-ACTION_BACKUPDEF='Y';                       # default value for creation backups first: Y|N
-
-# To install as a service set to 1 otherwise something different e.g 0
+# To install as a service set to 1 otherwise something different e.g: 0
 INSTALL_AS_SERVICE=1
 
 ACTION_ASKQUESTIONS='Y';                    # Ask or just use the config defaults (Y|N)? N for automisations
@@ -44,7 +38,12 @@ ACTION_TYPE='I';                            # Type of installation: I|U
 #GITEA_BIN_URL=https://dl.gitea.io/gitea/1.15.6/gitea-1.15.6-linux-amd64
 GITEA_BIN_URL=https://github.com/go-gitea/gitea/releases/download/v1.15.6/gitea-1.15.6-linux-amd64
 
-# --- internals ---
+# where this sources are located to fetch updates
+GITEA_INSTALLER_BASEURL='https://raw.githubusercontent.com/flobee/public/master/shell/gitea/install';
+
+
+###
+# internals
 
 DIRNAME_REPOSITORIES=`basename "${PATH_REPOSITORIES}"`
 
@@ -55,5 +54,6 @@ TIMENOW=`date +'%Y%m%d-%H%M%S'`;
 GITEA_BIN_BASENAME=`basename "$GITEA_BIN_URL"`;
 
 DIR_OF_FILE="$(dirname $(readlink -f "$0"))";
-# basis functions
+
+# source basic functions in several [sub]scripts
 . ${DIR_OF_FILE}/shellFunctions.sh;
