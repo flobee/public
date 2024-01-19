6c7858ad87d907ccdde6271fe6b1d658bf512ebf
flobee <flobee@debian.local>
2024-01-19 21:41:25 +0100

    Updates...
    - phpdocs header for cross scripts
    - start offering node version of this script
    - Updates individual git log config regarding blue color output for
      aliases: verbose llog, log, ll, last, llogxx(testing)
    - Improves CS

--- shell/git/mk_git_aliases.php
+++ shell/git/mk_git_aliases.php
@@ -1,26 +1,32 @@
 #!/usr/bin/env php
 <?php
 
-# Git alias generator
-#
-# @autor Florian Blasel
-#
-# Executes shell commands to update/remove or init git aliases to 
-# global ~/.gitconfig (your user).
-# When runnin as root: 
-# - 'system' (system wide (/etc)), 
-# - 'global' (current user (root), 
-# - 'local' (current repo)
-# can be handled.
-# To be used in bash or zsh... to have 'git aliases'
-#
-# Including own helper aliases for daily business.
-#
-# Examples what this script does:
-#   git config --global alias.visual '!gitk' # git visual 
-#   git config --global --unset alias.visual # dropped in 'drop' to unset
 
-function gitaliases($test)
+/**
+ * Git alias generator
+ *
+ * # @autor Florian Blasel
+ *
+ * Executes shell commands to update/remove or init git aliases to
+ * global ~/.gitconfig (your user)
+ * When runnin as root:
+ *  - 'system' (system wide (/etc)),
+ *  - 'global' (current user/ your user (you)),
+ *  - 'local' (current repo)
+ * can be handled.
+ * To be used in bash or zsh... to have 'git aliases'
+ *
+ * Including own helper aliases for daily business.
+ *
+ * Examples what this script does, eg:
+ *      git config --global alias.visual '!gitk' # git visual
+ *      git config --global --unset alias.visual # dropped in 'drop' to unset
+ *
+ * Requires: php-cli package (apt install php-cli)
+ */
+
+
+function gitaliases( $test, $configFile = false )
 {
     // workflow: cleanup -> or drop/ remove -> add aliases
     $aliases = array(
@@ -107,24 +113,24 @@ function gitaliases($test)
                 #'pop' => 'stash pop',
 
                 // --- showing logs ---
-                'verbose' => "log --graph --stat '--pretty=format:Author of %C(red)%h%Creset was %C(green)%an%Creset, %C(blue)%ar%Creset, message was:\n%s\n%b\n%Creset'",
+                'verbose' => "log --graph --stat '--pretty=format:Author of %C(red)%h%Creset was %C(green)%an%Creset, %C(auto,yellow)%ar%Creset, message was:\n%s\n%b\n%Creset'",
                 #'l' => 'log --format=\'%C(red)%h%Creset %C(green)%an%Creset - %C(yellow)%s%Creset\' --graph',
 
                 #202311: %cd respects --date setting, now iso strict
-                'll' => 'log --pretty=format:\'%C(red)%h%Creset%C(blue)%d %C(green)%an%Creset - %s %C(blue)(%cd)%Creset\' --graph --date=iso8601-strict',
+                'll' => 'log --pretty=format:\'%C(red)%h%Creset%C(blue)%d %C(green)%an%Creset - %s %C(yellow)(%cd)%Creset\' --graph --date=iso8601-strict',
 
                 #202311: %cd respects --date setting, now iso strict
-                'llog' => "log --graph --stat '--pretty=format:Author of %Cred%h%Creset was %C(green)%ae%Creset, %C(blue)%cd%Creset, message:\n%C(yellow)%s\n%b\n%Creset' --date=iso8601-strict",
+                'llog' => "log --graph --stat '--pretty=format:Author of %Cred%h%Creset was %C(green)%ae%Creset, %C(yellow)%cd%Creset, message:\n%C(auto)%s\n%b\n%Creset' --date=iso8601-strict",
 
                 //'last' => "log -5 --graph --stat --pretty=format:'Author of %Cblue%h%Creset was %C(yellow)%an%Creset, %C(blue)%ar%Creset, message was\n%C(yellow)%s\n%b\n%Creset'",
-                'last' => 'log -5 --pretty=format:\'%C(red)%h%Creset%C(blue)%d %C( green)%an%Creset - %s %C(blue)( %cr)%Creset\' --graph --date=relative',
+                'last' => 'log -5 --pretty=format:\'%C(red)%h%Creset%C(auto)%d %C( green)%an%Creset - %s %C(yellow)( %cr)%Creset\' --graph --date=relative',
 
                 // search for a term in full history
                 // More hints: https://www.w3docs.com/snippets/git/how-to-find-a-deleted-file-in-the-project-commit-history.html
                 'logfind' => '!git llog --all --full-history -- ',
 
                 // playground, #202311: %cd respects --date setting, now iso lazy
-                'llogxx' => "log --graph --stat '--pretty=format:Author of %Cred%h%Creset was %C(green)%ae%Creset, %C(blue)%cd%Creset, message:\n%C(yellow)%s\n%b\n%Creset' --date=iso",
+                'llogxx' => "log --graph --stat '--pretty=format:Author of %Cred%h%Creset was %C(green)%ae%Creset, %C(yellow)%cd%Creset, message:\n%C(auto)%s\n%b\n%Creset' --date=iso",
 
                 #'continue' => '!git add . && git rebase --continue',
                 #'url' => 'config --local --get-regexp remote\\.\\.\\*\\.url',
@@ -240,19 +246,18 @@ function gitaliases($test)
         }
     }
 
-    foreach ( $execlist as $cmd )
-    {
-        if ( $test === false )
-        {
+    foreach ( $execlist as $cmd ) {
+        if ( $test === false ) {
             $x = exec( $cmd, $data, $exitCode );
             if ( $x || $data || $exitCode > 5 ) {
                 echo 'Problem? cmd: ' . $cmd . PHP_EOL;
-                echo (empty($x) ? '0':$x) . " || data || $exitCode" . PHP_EOL;
+                echo (empty( $x ) ? '0' : $x) . " || data || $exitCode" . PHP_EOL;
             }
             if ( $exitCode == 255 ) {
-                echo 'ERROR: permission problem with: '. $cmd . PHP_EOL;
+                echo 'ERROR: permission problem with: ' . $cmd . PHP_EOL;
             }
-        } else {
+        }
+        else {
             echo $cmd . PHP_EOL;
         }
     }
@@ -260,10 +265,10 @@ function gitaliases($test)
 
 
 $test = true;
-if (@$_SERVER['argv'][1] == 'run') {
+if ( @$_SERVER['argv'][1] === 'run' ) {
     $test = false;
 } else {
-    echo '# To execute commands run: "php ' . basename(__FILE__) .' run"' . PHP_EOL;
+    echo '# To execute commands run: "php ' . basename( __FILE__ ) . ' run"' . PHP_EOL;
 }
 
 gitaliases( $test );
