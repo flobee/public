820c9745b140da7139e1440a764871039171e636
flobee <flobee@debian.local>
2024-03-13 23:30:32 +0100

    Adds bash seasons checks

--- shell/bash/bash_basics.sh
+++ shell/bash/bash_basics.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 #DIR_OF_FILE="$(dirname "$(readlink -f "$0")")";
-SCRIPT_FILENAME=$(basename "$0");
+SCRIPT_FILENAME=$(basename "$0")
 
 if [ -z "$BASH" ]; then
-   echo "Please run this script '$0' with bash";
-   echo "Call it like './${SCRIPT_FILENAME}' or 'bash ${SCRIPT_FILENAME}";
-   exit 1;
+   echo "Please run this script '$0' with bash"
+   echo "Call it like './${SCRIPT_FILENAME}' or 'bash ${SCRIPT_FILENAME}"
+   exit 1
 fi
-
 
 # ###
 # #display the process ID of the shell it’s running in.
 # echo $$; # eg the current interactiv shell eg. your zsh
 # bash -c 'echo $$' # problably died after execution but the pid of that bash process
-
 
 ###
 # /test.sh one two "three four"
@@ -40,8 +38,6 @@ fi
 # done
 # #
 
-
-
 ###
 # string replace
 
@@ -63,8 +59,6 @@ fi
 # echo "${message//[0-9]/X}"
 # # prints 'The secret code is XXXXX'
 
-
-
 ###
 # heredoc
 # var='xxx';
@@ -85,3 +79,18 @@ fi
 #
 # tput cols # n columns.
 # tput lines # n lines
+
+case $(date +%m-%d) in
+03-2[2-9] | 03-3* | 0[4-6]-[01]* | 06-2[012])
+    echo "spring"
+    ;;
+06-2[2-9] | 06-3* | 0[7-9]-[01]* | 09-2[012])
+    echo "summer"
+    ;;
+09-2[2-9] | 09-3* | 1[012]-[01]* | 12-2[012])
+    echo "autumn"
+    ;;
+12-2[2-9] | 12-3* | 0[1-3]-[01]* | 03-2[012])
+    echo "winter"
+    ;;
+esac
