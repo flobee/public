f4b1bb6635a1bbd86b6077d1c55c52adbbe22bed
flobee <flobee@debian.local>
2024-10-04 10:11:43 +0200

    Adds bash script for file/path  handling

---
+++ shell/bash/filesystem.sh
@@ -0,0 +1,35 @@
+#!/bin/dash
+
+getFile() {
+    basename "$1";
+}
+
+getFileName() {
+    basename "${1%.*}";
+}
+
+getFileExtension() {
+    echo "${1##*\.}";
+}
+
+getFileDirectory() {
+    dirname "$1";
+}
+
+
+# @returns True if file exists and is a regular file.
+fileExists() {
+    if [ -f "$1" ]; then
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+dirExists() {
+    if [ -f "$1" ]; then
+        return 0;
+    else
+        return 1;
+    fi
+}
